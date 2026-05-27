36b331103e8cc6e20ea88ea89b85d74a000891d3
Jeromekessie <Jerome.kessie@gmail.com>
2026-05-26 20:10:19 -0400

    Revert "Initial ICE -Jerome"
    This reverts commit 0044e98bf2c8ddccf2bb7787c4c88da7c8de214d.

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,8 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
-import java.util.Random;
-import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -14,55 +12,21 @@ import java.util.Scanner;
  */
 public class CardTrick {
     
-    
     public static void main(String[] args)
     {
-        Random random = new Random();
-        
         Card[] magicHand = new Card[7];
-        Card c1=new Card();
-        c1.setSuit("diamond");
-        c1.setValue(2);
-        
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            c.setValue(random.nextInt(13) + 1);
-            c.setSuit(Card.SUITS[random.nextInt(4)]);
-            magicHand[i] =c;
+            //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
-        Scanner input = new Scanner(System.in);
-        System.out.println("Pick a card value (1-13): ");
-        int userCardValue = input.nextInt();
-        System.out.println("Pick a suit (Hearts, Diamonds, Spades, Clubs): ");
-        String userCardSuit = input.next();
-        
-        Card userCard = new Card();
-        userCard.setValue(userCardValue);
-        userCard.setSuit(userCardSuit);
-        
-        boolean found = false;
-        for (Card card : magicHand) {
-            // Check value first
-            if (card.getValue() == userCardValue) {
-                // If value matches, check the suit
-                if (card.getSuit().equalsIgnoreCase(userCardSuit)) {
-                    found = true;
-                    break; // Stop searching once a match is found
-                }
-            }
-        }
-        
-        if (found) {
-            System.out.println("Congrats! Your card is in the magic hand!!!!.");
-        } else {
-            System.out.println("Sorry! your card is not in the magic hand.\nYou picked: "+ userCardValue+" of "+userCardSuit);
-            
-            System.out.println("\nThe magic hand contained:"); // added extra wanted to debug but i like that it shows me
-            for (Card c : magicHand) {
-                System.out.print(c.getValue() + " of " + c.getSuit()+",");}
-}
-    }    
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+        // add one luckcard hard code 2,clubs
+    }
+    
 }
